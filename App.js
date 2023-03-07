b57c011f49d4f874b22f3d5728c02c965664bd55
mustapirak36 <51321677+mustapirak36@users.noreply.github.com>
2023-03-07 09:48:01 +0700

    Update App.js

--- App.js
+++ App.js
@@ -4,19 +4,14 @@ import { AppRegistry, StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/stores';
 import Login from './src/screens/Login';
-import Home from './src/screens/Home';
 import Checkout from './src/screens/Checkout';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { name as appName } from "./app.json"; 
-import { connect } from 'react-redux';
-import Routes from './src/routers';
-
 import DrawerNavigator from './src/components/navigation/DrawerNavigator';
 import { useFonts } from 'expo-font';
 import Text from './src/components/Text';
-import {Ionicons} from '@expo/vector-icons';
-import Tabledetail from './src/screens/Tabledetail';
+import TableDetail from './src/screens/TableDetail';
 import Qrcode from './src/screens/Qrcode';
 import Camera from './src/screens/Camera';
 import HistoryBill from './src/screens/HistoryBill';
@@ -80,7 +75,7 @@ function App({route}) {
               fontSize: 14,
             }
           }}  />
-          <Stack.Screen name="TableDetail" component={Tabledetail}  options={Tabledetail.defaultNavigationOptions}/>
+          <Stack.Screen name="TableDetail" component={TableDetail}  options={TableDetail.defaultNavigationOptions}/>
           <Stack.Screen name="HistoryBill" component={HistoryBill}  options={HistoryBill.defaultNavigationOptions}/>
           <Stack.Screen name="Qrcode" component={Qrcode}  options={Qrcode.defaultNavigationOptions}/>
           <Stack.Screen name="Camera" component={Camera}  options={Camera.defaultNavigationOptions}/>
@@ -92,14 +87,6 @@ function App({route}) {
   );
 }
 AppRegistry.registerComponent(appName, () => App);
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App
 
